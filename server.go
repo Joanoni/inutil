6e157b557ac1d0ae88a30e741294ad80373115ad
Caio Joanoni <caio.joanoni@gmail.com>
2024-03-10 12:42:26 -0300

    patch

--- server.go
+++ server.go
@@ -1,11 +1,26 @@
 package inutil
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+	"time"
 
-var Server *mux.Router
+	"github.com/gorilla/mux"
+)
 
-func StartServer() {
-	Server = mux.NewRouter()
+var Router *mux.Router
+var Server *http.Server
+
+func StartServer(address string) {
+	Router = mux.NewRouter()
+
+	Server = &http.Server{
+		Addr: address, //"0.0.0.0:8080",
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      Router, // Pass our instance of gorilla/mux in.
+	}
 }
 
 func Oi() {
