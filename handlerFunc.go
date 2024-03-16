4458f367786b2afab485569fa2489bb4a77524d0
Caio Joanoni <caio.joanoni@gmail.com>
2024-03-16 15:12:41 -0300

    patch

--- handlerFunc.go
+++ handlerFunc.go
@@ -1,6 +1,8 @@
 package inutil
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 type HandlerFunc func(c *Context)
 
--- middleware.go
+++ middleware.go
@@ -4,14 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Middleware struct {
-}
-
-func (m *Middleware) Log(c HandlerFunc) HandlerFunc {
+func MiddlewareLog(c HandlerFunc) HandlerFunc {
 	return HandlerFunc(wrapperHandlerFromGin(gin.Logger()))
 }
 
-func (m *Middleware) Cors(c HandlerFunc) HandlerFunc {
+func MiddlewareCors(c HandlerFunc) HandlerFunc {
 	return HandlerFunc(func(c *Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
